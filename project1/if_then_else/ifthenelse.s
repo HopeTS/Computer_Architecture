da0c8000bed4d8f2f7bafe94d07e68bcbc6e03f1
Robby <bergersr@my.easternct.edu>
2019-10-06 18:52:38 -0400

    If then else conditional

---
+++ project1/if_then_else/ifthenelse.s
@@ -0,0 +1,18 @@
+.text
+.global main
+main:
+	/* Variables */
+	mov r1, #0
+	mov r2, #10
+if:
+	cmp r1, r2
+	blt then	/* If r1 < r2 go to then*/
+	bge else	/* Else go to else */
+then:
+	mov r0, #1
+	b end
+else:
+	mov r0, #2
+	b end
+end:
+	bx lr
